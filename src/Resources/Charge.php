6be4b8e94f301ff9a0dfd295e3a6df71b807c55b
Mateus Guimarães <mateus.jatene@gmail.com>
2016-07-03 04:53:20 -0300

    Charge

---
+++ src/Resources/Charge.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mateusjatenee\Iugu\Resources;
+
+use Artesaos\Restinga\Data\Resource;
+use Artesaos\Restinga\Http\Format\Receive\ReceiveJson;
+use Artesaos\Restinga\Http\Format\Receive\ReceiveJsonErrors;
+use Artesaos\Restinga\Http\Format\Send\SendJson;
+
+class Charge extends Resource
+{
+    use ReceiveJson, SendJson;
+
+    use ReceiveJsonErrors;
+
+    protected $service = 'iugu';
+
+    protected $name = 'charge';
+
+    protected $identifier = 'invoice_id';
+
+    protected $item_root = null;
+
+    public function charge(array $data)
+    {
+        $this->token = $data['token'];
+        $this->email = $data['email'];
+        $this->items = $data['items'];
+        $this->payer = isset($data['payer']) ? $data['payer'] : null;
+
+        $this->save();
+
+    }
+
+}
---
+++ tests/Resources/ChargeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Mateusjatenee\Iugu\Iugu;
+use Mateusjatenee\Iugu\Resources\Charge;
+use Mateusjatenee\Iugu\Tests\Traits\HelpersTrait;
+
+class ChargeTest extends \PHPUnit_Framework_TestCase
+{
+
+    use HelpersTrait;
+
+    /** @test */
+    public function it_can_charge_an_user()
+    {
+
+        Iugu::setApiKey(getenv('IUGU_API_KEY'));
+
+        $token = $this->generateToken();
+
+        $charge = new Charge();
+
+        $charge->charge([
+            'token' => $token->id,
+            'email' => 'teste@superteste.abc',
+            'items' => [
+                'description' => 'Item Teste',
+                'quantity' => '1',
+                'price_cents' => '100',
+            ],
+        ]);
+
+        var_dump($charge);
+
+        $this->assertNotNull($charge);
+        $this->assertNotNull($charge->invoice_id);
+        $this->assertEquals($charge->success, true);
+        $this->assertEquals($charge->message, 'Autorizado');
+        $this->assertNotNull($charge->url);
+        $this->assertNotNull($charge->pdf);
+
+    }
+}
