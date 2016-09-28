f600fd85c539400c1bffc159f2b75b81172ecabe
Mateus Guimarães <mateus.jatene@gmail.com>
2016-09-28 16:11:25 -0300

    customer payment methods test

--- tests/Resources/CustomerTest.php
+++ tests/Resources/CustomerTest.php
@@ -3,6 +3,7 @@
 namespace Mateusjatenee\Iugu\Tests\Resources;
 
 use Mateusjatenee\Iugu\Resources\Customer;
+use Mateusjatenee\Iugu\Resources\PaymentMethod;
 use Mateusjatenee\Iugu\Tests\TestCase;
 
 class CustomerTest extends TestCase
@@ -30,5 +31,15 @@ class CustomerTest extends TestCase
     public function it_has_the_correct_identifiers()
     {
         $this->assertEquals('customers', $this->customer->getResourceName());
+    }
+
+    /** @test */
+    public function it_has_payment_methods()
+    {
+        $payment_method = $this->customer->paymentMethods();
+
+        $this->assertInstanceOf(PaymentMethod::class, $payment_method);
+
+        $this->assertInstanceOf(Customer::class, $payment_method->getParentResource());
     }
 }
